392df095af98a1041119d97113f624bca4d768d0
L4vish1 <av3ry.lan3@gmail.com>
2025-09-21 13:53:01 -0700

    Initial Commit

---
+++ Main/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  Main
+//
+//  Created by av3ry on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
