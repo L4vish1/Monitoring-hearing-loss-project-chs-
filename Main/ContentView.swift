045a9e57ed93ac546e9fc96c7858b846dae83deb
L4vish1 <av3ry.lan3@gmail.com>
2026-03-01 21:10:55 -0800

    Tabs

--- Main/ContentView.swift
+++ Main/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    
+        TabView{
+            StreakView()
+                .tabItem{
+                    Image(systemName: "chart.bar")
+                    Text("Streak")
+                }
+            TestView()
+                .tabItem{
+                    Image(systemName: "headphones")
+                    Text("Test")
+                }
+            DataView()
+                .tabItem{
+                    Image(systemName: "heart")
+                    Text("Data")
+                }
+        }
     }
+}
+struct StreakView: View {
+    var body: some View {
+        Text("Streak")
+    }
+
+}
+struct TestView: View {
+    var body: some View {
+        Text("Test")
+    }
+
+}
+struct DataView: View {
+    var body: some View {
+        Text("Data")
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
